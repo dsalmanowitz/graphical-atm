f9106f44d615c24748580dc96d17f919f4c41a3e
dsalmanowitz <43217565+dsalmanowitz@users.noreply.github.com>
2018-12-20 18:24:47 -0500

    Update CreateView.java

--- view/CreateView.java
+++ view/CreateView.java
@@ -15,6 +15,7 @@ public class CreateView extends JPanel implements ActionListener {
 	
 	private ViewManager manager;		// manages interactions between the views, model, and database
 	private JButton submitButton;
+	private JButton quitButton;
 	private JTextField firstNameField;
 	private JTextField lastNameField;
 	private JComboBox bdayBox;
@@ -50,75 +51,101 @@ public class CreateView extends JPanel implements ActionListener {
 	private void initialize() {
 		
 		this.setLayout(null);
-		/*JLabel firstNameLabel = new JLabel("First Name", SwingConstants.RIGHT);
-		firstNameLabel.setBounds(100, 140, 95, 35);
+		
+		JLabel firstNameLabel = new JLabel("First Name", SwingConstants.RIGHT);
+		firstNameLabel.setBounds(30, 10, 95, 35);
 		firstNameLabel.setLabelFor(firstNameField);
 		firstNameLabel.setFont(new Font("DialogInput", Font.BOLD, 14));
-		this.add(firstNameLabel)*/
-		firstNameField = new JTextField("First Name");
-		firstNameField.setBounds(205, 180, 200, 35);
+		this.add(firstNameLabel);
+		firstNameField = new JTextField();
+		firstNameField.setBounds(140, 10, 200, 35);
 		this.add(firstNameField);
 		
-		/*JLabel lastNameLabel = new JLabel("Last Name", SwingConstants.RIGHT);
-		lastNameLabel.setBounds(100, 140, 95, 35);
+		JLabel lastNameLabel = new JLabel("Last Name", SwingConstants.RIGHT);
+		lastNameLabel.setBounds(30, 50, 95, 35);
 		lastNameLabel.setLabelFor(lastNameField);
 		lastNameLabel.setFont(new Font("DialogInput", Font.BOLD, 14));
-		this.add(lastNameLabel); */
-		lastNameField = new JTextField("Last Name");
-		lastNameField.setBounds(205, 150, 200, 35);
+		this.add(lastNameLabel);
+		lastNameField = new JTextField();
+		lastNameField.setBounds(140, 50, 200, 35);
 		this.add(lastNameField);
 		
-		/*JLabel bdayLabel = new JLabel("Birthday", SwingConstants.RIGHT);
-		bdayLabel.setBounds(100, 140, 95, 35);
+		JLabel bdayLabel = new JLabel("Birthday", SwingConstants.RIGHT);
+		bdayLabel.setBounds(30, 90, 95, 35);
 		bdayLabel.setLabelFor(lastNameField);
 		bdayLabel.setFont(new Font("DialogInput", Font.BOLD, 14));
-		this.add(bdayLabel); */
+		this.add(bdayLabel);
 		String[] days = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
 		bdayBox = new JComboBox(days);
-		bdayBox.setBounds(205, 180, 200, 35);
+		bdayBox.setBounds(140, 90, 50, 35);
 		this.add(bdayBox);
 		String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 		monthBox = new JComboBox(months);
-		monthBox.setBounds(205, 180, 200, 35);
+		monthBox.setBounds(190, 90, 150, 35);
 		this.add(monthBox);
 		
-		/*JLabel phoneLabel = new JLabel("Phone Number", SwingConstants.RIGHT);
-		phoneLabel.setBounds(100, 140, 95, 35);
+		JLabel phoneLabel = new JLabel("Phone #", SwingConstants.RIGHT);
+		phoneLabel.setBounds(30, 130, 95, 35);
 		phoneLabel.setLabelFor(phoneField);
 		phoneLabel.setFont(new Font("DialogInput", Font.BOLD, 14));
-		this.add(phoneLabel); */
-		phoneField = new JTextField("Phone #");
-		phoneField.setBounds(205, 180, 200, 35);
+		this.add(phoneLabel);
+		phoneField = new JTextField();
+		phoneField.setBounds(140, 130, 200, 35);
 		this.add(phoneField);
 		
-		/*JLabel addressLabel = new JLabel("Address");
-		addressLabel.setBounds(100, 140, 95, 35);
+		JLabel addressLabel = new JLabel("Address", SwingConstants.RIGHT);
+		addressLabel.setBounds(30, 170, 95, 35);
 		addressLabel.setLabelFor(addressField);
-		this.add(addressLabel);*/
-		addressField = new JTextField("Address");
-		addressField.setBounds(205, 180, 200, 35);
+		lastNameLabel.setFont(new Font("DialogInput", Font.BOLD, 14));
+		this.add(addressLabel);
+		addressField = new JTextField();
+		addressField.setBounds(140, 170, 200, 35);
 		this.add(addressField);
-		cityField = new JTextField("City");
+		
+		cityField = new JTextField();
+		JLabel cityLabel = new JLabel("City", SwingConstants.RIGHT);
+		cityLabel.setLabelFor(cityField);
+		lastNameLabel.setFont(new Font("DialogInput", Font.BOLD, 14));
+		cityLabel.setBounds(30, 210, 95 ,35);
+		cityField.setBounds(140, 210, 150, 35);
+		this.add(cityLabel);
 		this.add(cityField);
 		String[] states = {"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "WA", "WV", "WI", "WY"};
 		stateBox = new JComboBox(states);
+		stateBox.setBounds(290, 210, 50, 35);
 		this.add(stateBox);
-		zipField = new JTextField("ZIP");
+		
+		JLabel zipLabel = new JLabel("ZIP", SwingConstants.RIGHT);
+		lastNameLabel.setFont(new Font("DialogInput", Font.BOLD, 14));
+		zipLabel.setBounds(30, 250, 95, 35);
+		zipLabel.setLabelFor(zipField);
+		this.add(addressLabel);
+		zipField = new JTextField();
+		zipField.setBounds(140, 250, 50, 35);
+		this.add(zipLabel);
 		this.add(zipField);
 		
-		/*JLabel pinLabel = new JLabel("PIN", SwingConstants.RIGHT);
-		pinLabel.setBounds(100, 140, 95, 35);
-		pinLabel.setLabelFor(pinField);
+		JLabel pinLabel = new JLabel("PIN", SwingConstants.RIGHT);
 		pinLabel.setFont(new Font("DialogInput", Font.BOLD, 14));
-		this.add(pinLabel); */
-		pinField = new JTextField("PIN");
-		pinField.setBounds(205, 180, 200, 35);
+		pinLabel.setBounds(30, 290, 95, 35);
+		pinLabel.setLabelFor(pinField);
+		pinField = new JTextField();
+		pinField.setBounds(140, 290, 200, 35);
+		this.add(pinLabel); 
 		this.add(pinField);
 		
 		submitButton = new JButton("Submit");
-		submitButton.setBounds(205, 180, 200, 35);
+		submitButton.setFont(new Font("DialogInput", Font.BOLD, 14));
+		submitButton.setBounds(50, 390, 200, 35);
 		submitButton.addActionListener(this);
 		this.add(submitButton);
+		
+		quitButton = new JButton("Quit");
+		quitButton.setFont(new Font("DialogInput", Font.BOLD, 14));
+		quitButton.setBounds(260, 390, 200, 35);
+		quitButton.addActionListener(this);
+		this.add(quitButton);
+		
 	}
 	
 	/*
@@ -143,12 +170,12 @@ public class CreateView extends JPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		// TODO
-		//
-		// this is where you'll setup your action listener, which is responsible for
-		// responding to actions the user might take in this view (an action can be a
-		// user clicking a button, typing in a textfield, etc.).
-		//
-		// feel free to use my action listener in LoginView.java as an example.
+		Object source = e.getSource();
+		if (source.equals(quitButton)) {
+			manager.switchTo(ATM.LOGIN_VIEW);
+		}
+		if (source.equals(submitButton)) {
+			manager.switchTo(ATM.HOME_VIEW);
+		}
 	}
 }

3f215cdda075f2dabc2657c723d5c2c18a41dd57
dsalmanowitz <43217565+dsalmanowitz@users.noreply.github.com>
2019-01-10 11:54:07 -0500

    Add files via upload

---
+++ view/DepositView.java
@@ -0,0 +1,103 @@
+package view;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import controller.ViewManager;
+
+@SuppressWarnings("serial")
+public class DepositView extends JPanel implements ActionListener {
+	
+	private ViewManager manager;		// manages interactions between the views, model, and database
+	private JButton quitButton;
+	private JButton submitButton;
+	private JTextField amountField;
+	
+	/**
+	 * Constructs an instance (or objects) of the HomeView class.
+	 * 
+	 * @param manager
+	 */
+	
+	public DepositView(ViewManager manager) {
+		super();
+		
+		this.manager = manager;
+		initialize();
+	}
+	
+	///////////////////// PRIVATE METHODS /////////////////////////////////////////////
+	
+	/*
+	 * Initializes the HomeView components.
+	 */
+	
+	private void initialize() {
+		
+		this.setLayout(null);
+		
+		JLabel depositLabel = new JLabel("Deposit", SwingConstants.CENTER);
+		depositLabel.setBounds(140, 10, 200, 35);
+		this.add(depositLabel);
+		
+		JLabel amountLabel = new JLabel("Amount", SwingConstants.RIGHT);
+		amountLabel.setBounds(30, 50, 95, 35);
+		amountLabel.setLabelFor(amountField);
+		this.add(amountLabel);
+		amountField = new JTextField();
+		amountField.setBounds(140, 50, 200, 35);
+		this.add(amountField);
+		submitButton = new JButton("Submit");
+		submitButton.setBounds(140, 90, 200, 35);
+		submitButton.addActionListener(this);
+		this.add(submitButton);
+		quitButton = new JButton("Quit");
+		quitButton.setBounds(140, 130, 200, 35);
+		quitButton.addActionListener(this);
+		this.add(quitButton);
+	}
+	
+	
+	/*
+	 * HomeView is not designed to be serialized, and attempts to serialize will throw an IOException.
+	 * 
+	 * @param oos
+	 * @throws IOException
+	 */
+	
+	private void writeObject(ObjectOutputStream oos) throws IOException {
+		throw new IOException("ERROR: The HomeView class is not serializable.");
+	}
+	
+	///////////////////// OVERRIDDEN METHODS //////////////////////////////////////////
+	
+	/*
+	 * Responds to button clicks and other actions performed in the HomeView.
+	 * 
+	 * @param e
+	 */
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		if (source.equals(submitButton)) {
+			double amount = Double.parseDouble(amountField.getText());
+			manager.deposit(amount);
+			manager.switchTo(ATM.HOME_VIEW);
+		}
+		if (source.equals(quitButton)) {
+			manager.switchTo(ATM.HOME_VIEW);
+
+		}
+	}
+}
---
+++ view/TransferView.java
@@ -0,0 +1,120 @@
+package view;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.text.ParseException;
+
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.text.MaskFormatter;
+
+import controller.ViewManager;
+
+@SuppressWarnings("serial")
+public class TransferView extends JPanel implements ActionListener {
+	
+	private ViewManager manager;		// manages interactions between the views, model, and database
+	private JButton quitButton;
+	private JButton submitButton;
+	private JTextField amountField;
+	private JFormattedTextField destinationField;
+	
+	/**
+	 * Constructs an instance (or objects) of the HomeView class.
+	 * 
+	 * @param manager
+	 */
+	
+	public TransferView(ViewManager manager) {
+		super();
+		
+		this.manager = manager;
+		initialize();
+	}
+	
+	///////////////////// PRIVATE METHODS /////////////////////////////////////////////
+	
+	/*
+	 * Initializes the HomeView components.
+	 */
+	
+	private void initialize() {
+		
+		this.setLayout(null);
+		
+		JLabel transferLabel = new JLabel("Transfer Funds", SwingConstants.CENTER);
+		transferLabel.setBounds(140, 10, 200, 35);
+		this.add(transferLabel);
+		
+		JLabel amountLabel = new JLabel("Amount", SwingConstants.RIGHT);
+		amountLabel.setBounds(30, 50, 95, 35);
+		amountLabel.setLabelFor(amountField);
+		this.add(amountLabel);
+		amountField = new JTextField();
+		amountField.setBounds(140, 50, 200, 35);
+		this.add(amountField);
+		JLabel destinationLabel = new JLabel("Destination", SwingConstants.RIGHT);
+		destinationLabel.setBounds(30, 170, 95, 35);
+		destinationLabel.setLabelFor(destinationField);
+		this.add(destinationLabel);
+		try {
+			MaskFormatter phoneFormat = new MaskFormatter("(###) ###-####");
+			phoneFormat.setPlaceholderCharacter('_');
+			destinationField = new JFormattedTextField(phoneFormat);
+		} catch (ParseException e) {
+			destinationField.setText("");
+		}
+		destinationField.setBounds(140, 170, 200, 35);
+		this.add(destinationField);
+		submitButton = new JButton("Submit");
+		submitButton.setBounds(140, 90, 200, 35);
+		submitButton.addActionListener(this);
+		this.add(submitButton);
+		quitButton = new JButton("Quit");
+		quitButton.setBounds(140, 130, 200, 35);
+		quitButton.addActionListener(this);
+		this.add(quitButton);
+	}
+	
+	
+	/*
+	 * HomeView is not designed to be serialized, and attempts to serialize will throw an IOException.
+	 * 
+	 * @param oos
+	 * @throws IOException
+	 */
+	
+	private void writeObject(ObjectOutputStream oos) throws IOException {
+		throw new IOException("ERROR: The HomeView class is not serializable.");
+	}
+	
+	///////////////////// OVERRIDDEN METHODS //////////////////////////////////////////
+	
+	/*
+	 * Responds to button clicks and other actions performed in the HomeView.
+	 * 
+	 * @param e
+	 */
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		if (source.equals(submitButton)) {
+			double amount = Double.parseDouble(amountField.getText());
+			manager.deposit(amount);
+			manager.switchTo(ATM.HOME_VIEW);
+		}
+		if (source.equals(quitButton)) {
+			manager.switchTo(ATM.HOME_VIEW);
+
+		}
+	}
+}

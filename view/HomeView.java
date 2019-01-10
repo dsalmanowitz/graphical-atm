1120fdde494cf09210169c180ee1d284895eab66
dsalmanowitz <43217565+dsalmanowitz@users.noreply.github.com>
2019-01-10 11:52:52 -0500

    Update HomeView.java

--- view/HomeView.java
+++ view/HomeView.java
@@ -7,6 +7,8 @@ import java.io.ObjectOutputStream;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import controller.ViewManager;
 
@@ -15,6 +17,9 @@ public class HomeView extends JPanel implements ActionListener {
 	
 	private ViewManager manager;		// manages interactions between the views, model, and database
 	private JButton logoutButton;
+	private JButton depositButton;
+	private JButton withdrawButton;
+	private JButton transferButton;
 	
 	/**
 	 * Constructs an instance (or objects) of the HomeView class.
@@ -37,18 +42,33 @@ public class HomeView extends JPanel implements ActionListener {
 	
 	private void initialize() {
 		
-		// TODO
-		//
-		// this is a placeholder for this view and should be removed once you start
-		// building the HomeView.
+		this.setLayout(null);
 		
-		this.add(new javax.swing.JLabel("HomeView", javax.swing.SwingConstants.CENTER));
-		
+		/*this.add(new javax.swing.JLabel("Account Number: " + manager.getAcc().getAccountNumber(), javax.swing.SwingConstants.CENTER));
+		this.add(new javax.swing.JLabel("First Name: " + manager.getAcc().getUser().getFirstName(), javax.swing.SwingConstants.CENTER));
+		this.add(new javax.swing.JLabel("Last Name: " + manager.getAcc().getUser().getLastName(), javax.swing.SwingConstants.CENTER));
+		this.add(new javax.swing.JLabel("Balance: " + manager.getAcc().getBalance(), javax.swing.SwingConstants.CENTER));
+		*/
+		depositButton = new JButton("Deposit");
+		depositButton.setBounds(140, 50, 200, 35);
+		depositButton.addActionListener(this);
+		this.add(depositButton);
+		withdrawButton = new JButton("Withdraw");
+		withdrawButton.setBounds(140, 90, 200, 35);
+		withdrawButton.addActionListener(this);
+		this.add(withdrawButton);
+		transferButton = new JButton("Transfer");
+		transferButton.setBounds(140, 130, 200, 35);
+		transferButton.addActionListener(this);
+		this.add(transferButton);
 		logoutButton = new JButton("Logout");
-		logoutButton.setBounds(205, 180, 200, 35);
+		logoutButton.setBounds(140, 170, 200, 35);
 		logoutButton.addActionListener(this);
-		
 		this.add(logoutButton);
+	}
+	
+	private void getInfo() {
+		
 	}
 	
 	/*
@@ -70,6 +90,7 @@ public class HomeView extends JPanel implements ActionListener {
 	 * @param e
 	 */
 	
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object source = e.getSource();
@@ -77,6 +98,17 @@ public class HomeView extends JPanel implements ActionListener {
 			manager.logout();
 			manager.switchTo(ATM.LOGIN_VIEW);
 		}
-		
+		if (source.equals(depositButton)) {
+			manager.switchTo(ATM.DEPOSIT_VIEW);
+
+		}
+		if (source.equals(withdrawButton)) {
+			manager.switchTo(ATM.WITHDRAW_VIEW);
+
+		}
+		if (source.equals(transferButton)) {
+			manager.switchTo(ATM.TRANSFER_VIEW);
+
+		}
 	}
 }
